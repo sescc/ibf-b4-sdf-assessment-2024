03f1f594a9d0db5d706ddea89e76f19578c0496f
sescc <codingscc@gmail.com>
2024-03-01 09:07:05 +0800

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,57 @@
+
+import java.io.IOException;
+
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        // Run Your Code here
+
+    }
+
+    public static void clearConsole() throws IOException {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    // Task 1
+    public static void pressAnyKeyToContinue() {
+        // your code here
+    }
+
+    // Task 1
+    public static void printHeader() {
+
+        // Task 1 - your code here
+    }
+
+    // Task 1
+    public static void printExitMessage() {
+
+        // Task 1 - your code here
+    }
+
+    // Task 1
+    public static void savePokemonStack(String pokemonStack, String filename) {
+
+        // Task 1 - your code here
+    }
+
+    // Task 2
+    public static void printUniquePokemonStack(Integer stack) {
+        // Task 2 - your code here
+    }
+
+    // Task 2
+    public static void printNext5StarsPokemon(String enteredPokemon) {
+        // Task 2 - your code here
+
+    }
+
+    // Task 2
+    public static void printPokemonCardCount() {
+        // Task 2 - your code here
+    }
+
+}
---
+++ src/FileService.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class FileService {
+    public List<String> ReadCSV(String fullPathFilename) {
+        // Task 1 - your code here
+
+        return null;
+    }
+
+    public void writeAsCSV(String pokemons, String fullPathFilename) {
+        // Task 1 - your code here
+
+    }
+}
