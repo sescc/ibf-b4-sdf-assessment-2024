80c2421fcb941d77e8fb789efc481c9daebed65e
sescc <codingscc@gmail.com>
2024-03-01 10:02:25 +0800

    Added quit and options skeleton logic.

--- src/myapp/App.java
+++ src/myapp/App.java
@@ -3,8 +3,15 @@ package myapp;
 import java.io.Console;
 import java.io.IOException;
 
-
 public class App {
+    /*
+     * TODO-refactor
+     * public static final char QUIT = 'q';
+     * public static final int ONE = 1;
+     * public static final int TWO = 2;
+     * public static final int THREE = 3;
+     * public static final int FOUR = 4;
+     */
 
     public static void main(String[] args) throws Exception {
 
@@ -17,16 +24,48 @@ public class App {
         System.out.println("(3) Create new Pokemon stack and save (append) to csv file");
         System.out.println("(4) Print distinct Pokemon and cards count");
         System.out.println("(q) to exit the program");
-        
+
         Console cons = System.console();
         String input = cons.readLine("Enter your selection >");
+
+        // TODO: option block
+        input = input.trim().toLowerCase();
+        while (!"q".equals(input)) {
+
+            switch (input) {
+                // case "q":
+                // break;
+                case "1":
+                    // TODO
+                    System.out.println("Option 1");
+                    break;
+
+                case "2":
+                    // TODO
+                    System.out.println("Option 2");
+                    break;
+
+                case "3":
+                    // TODO
+                    System.out.println("Option 3");
+                    break;
+
+                case "4":
+                    // TODO
+                    System.out.println("Option 4");
+                    break;
+
+                default:
+                    System.err.println("Invalid option. Please try again.");
+                    break;
+
+            }
+            input = cons.readLine("Enter your selection >").trim().toLowerCase();;
+
+        }
+
+        // TODO:toremove - main options end
         System.out.println();
-
-        //TODO: option block
-
-
-
-        //TODO:toremove - main options end
         System.out.println("Thank you for using the program...");
         System.out.println("Hope to see you soon...");
         // Menu block ends
