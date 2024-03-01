e1fb3cf82c20da601dadc0a3a96311800d6acb74
sescc <codingscc@gmail.com>
2024-03-01 11:47:54 +0800

    Implemented writeAsCSV()

--- src/myapp/FileService.java
+++ src/myapp/FileService.java
@@ -1,8 +1,12 @@
 package myapp;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
@@ -48,7 +52,46 @@ public class FileService {
 
     public void writeAsCSV(String pokemons, String fullPathFilename) {
         // Task 1 - your code here
-        
+        //File file = Paths.get(fullPathFilename).toFile();
+        File file = new File(fullPathFilename);
 
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fullPathFilename, true))) {
+            if (!file.isDirectory()) {
+                if (!file.exists()) {
+                    // TODO: check if directory exists
+                    file.createNewFile();
+                    System.out.printf("File %s created.\n", fullPathFilename);
+                }
+                bw.write(pokemons);
+                bw.newLine();
+                System.out.println("New Pokemon stack added.");
+            }
+                    
+                    //String line = br.readLine(); // reading 1st row
+                    //line = br.readLine(); // read data from first row onwards
+
+                    //while (line != null) {
+        /*TODO: put in writingCSV method
+        String[] pokemon = line.split(",");
+
+        for (int i = 0; i < pokemon.length; i++) {
+            pokemonList.add(pokemon[i]);
+        }
+        */
+                //     pokemonList.add(line);
+                //     line = br.readLine();
+                // }
+
+                // br.close();
+                // } catch (IOException e) {
+                //     e.printStackTrace();
+                // }
+                //}
+            else {
+                System.err.println("Please enter a valid filename. No action taken.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
